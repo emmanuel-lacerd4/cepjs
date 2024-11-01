5352cb7f2c1453519b9be92513546137a474eaf2
Emmanuel L. Nogueira <emmanuellacerd4@gmail.com>
2024-10-31 21:49:28 -0300

    cepjs

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
         .then(() => {
-            console.log("Servicer Worker registrado.")
+            console.log("Service Worker registrado.")
         })
 }
 
